ae084a1419be8ae824573b33bcd4416910d8e137
irasstar <valendaanatoli@gmail.com>
2020-03-30 19:53:08 +0300

    first commit

---
+++ kufar_api.py
@@ -0,0 +1,6 @@
+# kufar.by api url:
+# https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-paginated
+# kufar.by api parameters:
+# ?query=iphone&ot=1&cat=17010&size=200&lang=ru&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6M30=
+
+import requests
