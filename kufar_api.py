4d756aacce10b4bab594268fdbd5fe615b8d2987
irasstar <valendaanatoli@gmail.com>
2020-03-30 21:32:56 +0300

    core init implement

--- kufar_api.py
+++ kufar_api.py
@@ -4,3 +4,19 @@
 # ?query=iphone&ot=1&cat=17010&size=200&lang=ru&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6M30=
 
 import requests
+
+
+class Core:
+    def __init__(self):
+        url = 'https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-paginated'
+        params = {
+            'query': '',
+            'ot': 1,
+            'cat': 17010,
+            'size': 200,  # max size = 200
+            'lang': 'ru',
+            'cursor': '',  # page token (set here next page token from paginagion dict in response)
+
+        }
+
+
