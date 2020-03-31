4343b17ccc371d67c6b52d1dfeb39781276c0ed5
irasstar <valendaanatoli@gmail.com>
2020-03-31 14:19:47 +0300

    readmemd creation

--- kufar_api.py
+++ kufar_api.py
@@ -1,7 +1,18 @@
 # kufar.by api url:
+
+# search
 # https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-paginated
-# kufar.by api parameters:
+# parameters:
 # ?query=iphone&ot=1&cat=17010&size=200&lang=ru&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6M30=
+
+# ads count request
+# https://cre-api.kufar.by/ads-search/v1/engine/v1/search/count
+# parameters
+# ?cur=BYR&rgn=7&size=42&sort=lst.d&query=
+
+# ad user info
+# https://www.kufar.by/item/api/aduserinfo/2499605
+# 2499605 - user id
 
 import requests
 
@@ -18,5 +29,3 @@ class Core:
             'cursor': '',  # page token (set here next page token from paginagion dict in response)
 
         }
-
-
