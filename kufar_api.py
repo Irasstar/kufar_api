f25e904157fbe8b72e4053e8fb5e6bfd77ad6536
irasstar <valendaanatoli@gmail.com>
2020-04-01 09:05:46 +0300

    add get_data()

--- kufar_api.py
+++ kufar_api.py
@@ -4,28 +4,45 @@
 # https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-paginated
 # parameters:
 # ?query=iphone&ot=1&cat=17010&size=200&lang=ru&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6M30=
+# cursor - current page token
+# size - ads quantity
+# cat - category
 
 # ads count request
 # https://cre-api.kufar.by/ads-search/v1/engine/v1/search/count
 # parameters
 # ?cur=BYR&rgn=7&size=42&sort=lst.d&query=
 
-# ad user info
+# ad user info request
 # https://www.kufar.by/item/api/aduserinfo/2499605
 # 2499605 - user id
+
+# get image request
+# https://yams.kufar.by/api/v1/kufar-ads/images/51/5189867070.jpg?rule=gallery
+# rule=gallery - full size image
+# rule=line_thumbs - small image
+# 5189867070.jpg - image name
+# /51/ - first two number of image name
 
 import requests
 
 
 class Core:
     def __init__(self):
-        url = 'https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-paginated'
-        params = {
+        self.url = 'https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-paginated'
+        self.params = {
             'query': '',
             'ot': 1,
-            'cat': 17010,
+            'cat': 17010,  # search category
             'size': 200,  # max size = 200
             'lang': 'ru',
             'cursor': '',  # page token (set here next page token from paginagion dict in response)
 
         }
+    def get_data(self):
+        response = requests.get(self.url, self.params)
+        print(response.content.decode())
+
+if __name__ == "__main__":
+    my_core = Core()
+    my_core.get_data()
